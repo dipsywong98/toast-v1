49153de62759458b89074bf39f2b037bbef12583
Dipsy Wong <ycwongal@connect.ust.hk>
2023-12-03 18:28:41 +0800

    add exit on error

--- copy.sh
+++ copy.sh
@@ -1,9 +1,11 @@
+set -e
 url=$1 # "https://w5.ab.ust.hk/wcq/cgi-bin/2320/"
 path=$2 # "example"
 limit=1000000000000
 
+mkdir $path
 echo "copy $url to $path"
-echo "copy $url to $path" > log.txt
+echo "copy $url to $path" > $path/log.txt
 
 # if [ -d "$path" ]; then
 # httrack $url --path $path --update --verbose --robots=0 --advanced-progressinfo -#L$limit

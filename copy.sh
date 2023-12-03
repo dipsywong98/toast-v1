852a11009a467b2c7bc143f85989b6c24de5cb8d
Dipsy Wong <ycwongal@connect.ust.hk>
2023-12-03 18:35:55 +0800

    copy class quota schedule

--- copy.sh
+++ copy.sh
@@ -5,16 +5,16 @@ limit=1000000000000
 
 mkdir $path
 echo "copy $url to $path"
-echo "copy $url to $path" > $path/log.txt
 
-# if [ -d "$path" ]; then
-# httrack $url --path $path --update --verbose --robots=0 --advanced-progressinfo -#L$limit
-#   else
-# httrack $url --path $path --verbose --robots=0 --advanced-progressinfo -#L$limit
-# fi
+if [ -d "$path" ]; then
+httrack $url --path $path --verbose --robots=0 --advanced-progressinfo -#L$limit --update
+  else
+httrack $url --path $path --verbose --robots=0 --advanced-progressinfo -#L$limit
+fi
 
 git config --global user.name "(bot) Dipsy Wong"
 git config --global user.email "ycwongal@connect.ust.hk"
 git add $path
 git commit -m "[bot] Updated $url"
+git rebase
 git push
